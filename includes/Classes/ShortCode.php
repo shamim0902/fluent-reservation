abcd050be745319f3ea5740a9ac88e7c9cc3bed7
Nuhel <nuheljft@gmail.com>
2024-01-16 15:02:21 +0600

    Wip

--- includes/Classes/ShortCode.php
+++ includes/Classes/ShortCode.php
@@ -2,17 +2,24 @@
 
 namespace fluentReservation\Classes;
 
+use fluentReservation\Models\Rooms;
+
 class ShortCode
 {
     public function register()
     {
-        add_shortcode('fluent_products', function ($shortcodeAttributes) {
+        add_shortcode('fluent_reservation', function ($shortcodeAttributes) {
             return $this->render($shortcodeAttributes);
         });
     }
 
     public function render($shortcodeAttributes): string
     {
-        return "";
+        $availableRooms = (new Rooms())->getBookableRooms();
+        echo "<pre>";
+        var_dump($availableRooms);
+        echo "</pre>";
+        die();
+        return "Hello";
     }
 }
