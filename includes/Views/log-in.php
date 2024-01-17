3f548351eab89bc13117f9393c6cbc9dc712443e
Nuhel <nuheljft@gmail.com>
2024-01-17 15:01:36 +0600

    Wip.

--- includes/Views/log-in.php
+++ includes/Views/log-in.php
@@ -1,4 +1,4 @@
 <h3 class="text-green-700">Please log in to Book your room!</h3>
 <p>
-    <a href="<?php echo site_url('/wp-login.php?action=register&redirect_to=' . get_permalink()); ?>">Register</a>
+    <a href="<?php echo site_url('/wp-login.php'); ?>">Login</a>
 </p>
