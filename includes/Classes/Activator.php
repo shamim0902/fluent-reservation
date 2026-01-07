d5969abc35ccba82c7b2695dc72750cba8bc2b75
Nuhel <nuheljft@gmail.com>
2026-01-07 13:42:54 +0600

    Update Activator.php

--- includes/Classes/Activator.php
+++ includes/Classes/Activator.php
@@ -56,7 +56,7 @@ class Activator
         $table_name = $wpdb->prefix . 'fluent_reservation_rooms';
         $sql = "CREATE TABLE $table_name (
             id int(10) NOT NULL AUTO_INCREMENT,
-            room_no VARCHAR(20) NOT NULL,
+            room_no VARCHAR(200) NOT NULL,
             floor_no VARCHAR(20) NOT NULL,
             total_seat int(10) NOT NULL,
             status VARCHAR(20) NOT NULL,
